21164f971209181ead21b28a9ff890adc77ae7ef
Paolo <str237@gmail.com>
2022-04-14 22:07:45 +0200

    Running a simple test

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader};
 
 type DynResult<T> = Result<T, Box<dyn std::error::Error>>;
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 struct Point {
   x: usize,
   y: usize,
 }
 
+impl From<(usize, usize)> for Point {
+  fn from(input: (usize, usize)) -> Self {
+    Self {
+      x: input.0,
+      y: input.1,
+    }
+  }
+}
+
+impl Point {
+  pub fn new(x: usize, y: usize) -> Self {
+    Self { x, y }
+  }
+}
+
 #[derive(Debug, Default)]
 struct GameMap {
   data: Vec<Vec<char>>,
-  dimensions: Point
+  dimensions: Point,
 }
 
 impl GameMap {
@@ -19,25 +34,24 @@ impl GameMap {
     let f = File::open(path)?;
     let reader = BufReader::new(f);
     let mut line_iter = reader.lines();
-    let gm = GameMap::default();
-    let row: usize = 0;
+    let mut gm = GameMap::default();
     while let Some(Ok(l)) = line_iter.next() {
-      let v : Vec<char> = Vec::with_capacity(l.len());
-      let v:  Vec<char> = l.chars().collect();
-      for c in l.chars() {
-        gm.data
-      }
+      let v: Vec<char> = l.chars().collect();
+      gm.data.push(v);
     }
+    gm.dimensions = Point::from((gm.data.len(), gm.data[0].len()));
     Ok(gm)
   }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::GameMap;
+  use crate::{GameMap, Point};
 
+  #[test]
   fn read_simple_map() {
-    let gm = GameMap::read_from_path("./resources/map_simple.map").expect("map should be read properly");
-
+    let gm =
+      GameMap::read_from_path("./resources/map_simple.map").expect("map should be read properly");
+    assert_eq!(gm.dimensions, Point::new(4, 4));
   }
 }
