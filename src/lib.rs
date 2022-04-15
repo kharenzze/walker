022cfcc781c886ae9f0a9b63e5c3773bb38c8b06
Paolo <str237@gmail.com>
2022-04-15 19:04:55 +0200

    Cell type conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 mod point;
 
+use point::Point;
+use std::convert::TryFrom;
+use std::fmt::Display;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use point::Point;
+use thiserror::Error;
 
 type DynResult<T> = Result<T, Box<dyn std::error::Error>>;
 
@@ -12,23 +15,53 @@ pub struct AppConfig {
 
 impl AppConfig {
   pub fn new(path: String) -> Self {
-    Self {
-      path
-    }
+    Self { path }
   }
 }
 
 pub struct App;
 
 impl App {
-  pub fn run(config: AppConfig) {
-  }
+  pub fn run(config: AppConfig) {}
 }
 
 #[derive(Debug, Default)]
 struct GameMap {
   data: Vec<Vec<char>>,
   dimensions: Point,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum CellType {
+  Wall,
+  Floor,
+}
+
+#[derive(Debug, Error, PartialEq, Eq)]
+enum ConversionError {
+  #[error("Could not convert char '{0}' into CellType")]
+  CellType(char),
+}
+
+impl TryFrom<char> for CellType {
+  type Error = ConversionError;
+
+  fn try_from(value: char) -> Result<Self, Self::Error> {
+    match value {
+      '#' => Ok(CellType::Wall),
+      '.' => Ok(CellType::Floor),
+      _ => Err(ConversionError::CellType(value)),
+    }
+  }
+}
+
+impl From<CellType> for char {
+  fn from(value: CellType) -> Self {
+    match value {
+      CellType::Wall => '#',
+      CellType::Floor => '.',
+    }
+  }
 }
 
 impl GameMap {
@@ -46,14 +79,33 @@ impl GameMap {
   }
 }
 
+impl Display for GameMap {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    todo!()
+  }
+}
+
 #[cfg(test)]
 mod tests {
-  use crate::{GameMap, Point};
+  use crate::{CellType, GameMap, Point, ConversionError};
 
   #[test]
   fn read_simple_map() {
     let gm =
       GameMap::read_from_path("./resources/map_simple.map").expect("map should be read properly");
     assert_eq!(gm.dimensions, Point::new(4, 4));
+  }
+
+  #[test]
+  fn cell_type_conversion() {
+    let c: char = CellType::Floor.into();
+    assert_eq!(c, '.');
+    let c: char = CellType::Wall.into();
+    assert_eq!(c, '#');
+
+    let cell = CellType::try_from('.');
+    assert_eq!(Ok(CellType::Floor), cell);
+    let cell = CellType::try_from('K');
+    assert_eq!(Err(ConversionError::CellType('K')), cell);
   }
 }
